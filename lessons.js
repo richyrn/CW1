3b44c7fcb574c66c3cac36445f189977b4dd0064
Richard Adegorite <richardadegorite@gmail.com>
2024-03-29 18:27:47 +0000

    Initial commit

---
+++ lessons.js
@@ -0,0 +1,93 @@
+let lessons = [
+    {
+        "id": 1001,
+        "image": "images/board.jpg",
+        "subject": "Board games",
+        "price": 100.00,
+        "location": "Copenhagen",
+        "description": "This lesson is for students who find interest in playing Chess and board games",
+        "availability": 5
+    },
+    {
+        "id": 1002,
+        "image": "images/Tennis.jpg",
+        "subject": "Tennis 101",
+        "price": 700.00,
+        "location": "Sydney",
+        "description": "This is an extra lesson session for students who wish to improve their Tennis playing skills",
+        "availability": 5
+        
+    },
+    {
+        "id": 1003,
+        "image": "images/B-ball.jpg",
+        "subject": "Basketball 101",
+        "price": 90.00,
+        "location": "Barcelona",
+        "description": "This is a session for students who wish to improve their Basketball playing skills",
+        "availability": 5
+    },
+    {
+        "id": 1004,
+        "image": "images/english.jpg",
+        "subject": "English",
+        "price": 70.00,
+        "location": "Orlando",
+        "description": "Join us and improve your English Language skills",
+        "availability": 5
+    },
+    {
+        "id": 1005,
+        "image": "images/math.jpg",
+        "subject": "Maths",
+        "price": 500.00,
+        "location": "Dubai",
+        "description": "This course is for students who wish to improve their Mathematical understanding!.",
+        "availability": 5
+    },
+    {
+        "id": 1006,
+        "image": "images/cooding.jpg",
+        "subject": "Computer-Science",
+        "price": 300.00,
+        "location": "London",
+        "description": "Come to expand your knowledge on the fundamentals of web and app Development",
+        "availability": 5
+    },
+    {
+        "id": 1007,
+        "image": "images/art.jpg",
+        "subject": "Fine-Art",
+        "price": 200.00,
+        "location": "Abu-Dhabi",
+        "description": "This session is for students with little to no drawing skills to learn basic of art",
+        "availability": 5
+    },
+    {
+        "id": 1008,
+        "image": "images/physics.jpg",
+        "subject": "Physics",
+        "price": 30.00,
+        "location": "Glasgow",
+        "description": "This is a make up classes for the Physics lectures",
+        "availability": 5
+    },
+    {
+        "id": 1009,
+        "image": "images/football.jpg",
+        "subject": "Football 101",
+        "price": 900.00,
+        "location": "Manchester",
+        "description": "Come for the basic tutoring on how to play Football",
+        "availability": 5
+    },
+    {
+        "id": 1010,
+        "image": "images/music.jpg",
+        "subject": "Music",
+        "price": 400.00,
+        "location": "Houston",
+        "description": "This is for all Music Fanatics, come for tutoring in music.",
+        "availability": 5
+    }
+]
